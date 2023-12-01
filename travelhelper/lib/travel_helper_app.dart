941d332701545d0f2da023ef00a61ec4120d9ce5
Melugsam <130920105+Melugsam@users.noreply.github.com>
2023-12-01 16:12:54 +0300

    TravelHelperApp

--- travelhelper/lib/travel_helper_app.dart
+++ travelhelper/lib/travel_helper_app.dart
@@ -1,10 +1,26 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';  
 
 class TravelHelperApp extends StatelessWidget {
   const TravelHelperApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedTextKit(
+          animatedTexts: [
+            RotateAnimatedText('Travel Helper', textStyle: GoogleFonts.robotoSlab(color: Colors.deepPurple.shade600, fontSize: 30.0, decoration: TextDecoration.none)),
+            RotateAnimatedText('Travel Helper', textStyle: GoogleFonts.rubik(color: Colors.deepPurple.shade600, fontSize: 30.0, decoration: TextDecoration.none)),
+            RotateAnimatedText('Travel Helper', textStyle: GoogleFonts.playfairDisplay(color: Colors.deepPurple.shade600, fontSize: 30.0, decoration: TextDecoration.none)),
+          ],
+          
+          isRepeatingAnimation: true,
+          repeatForever: true,
+        )
+      ],
+    );
   }
 }
